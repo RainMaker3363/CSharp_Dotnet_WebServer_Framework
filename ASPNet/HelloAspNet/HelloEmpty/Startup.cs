20370609e79da14caa99885712c7ceb002ae0e78
RainMaker3363 <jsc22637@gmail.com>
2020-09-18 01:39:15 +0900

    My Work 2
    MMO Web Server Prac !

--- ASPNet/HelloAspNet/HelloEmpty/Startup.cs
+++ ASPNet/HelloAspNet/HelloEmpty/Startup.cs
@@ -16,6 +16,7 @@ namespace HelloEmpty
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +31,10 @@ namespace HelloEmpty
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                //endpoints.MapControllerRoute(
+                //    name: "default",
+                //    pattern: "{controller=Home}/{action=index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
